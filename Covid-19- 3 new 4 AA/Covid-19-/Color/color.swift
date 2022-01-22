2358563c609fbe66167121c9cd3b3accdf19daa4
alanood <alanood@MacBook-Air-alanood.local>
2022-01-22 17:37:23 +0300

    app icon

--- Covid-19- 3 new 4 AA/Covid-19-/Color/color.swift
+++ Covid-19- 3 new 4 AA/Covid-19-/Color/color.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIColor {
     static let covidBackground           = UIColor(named: "backgroundColor")!
     static let covidText                 = UIColor(named: "textColor")!
-    static let covidPurple               = UIColor(#colorLiteral(red: 0.9327735901, green: 0.7295225263, blue: 0.6845949292, alpha: 1))
+    static let covidPurple               = UIColor(#colorLiteral(red: 0.9368770719, green: 0.7487493753, blue: 0.7164307237, alpha: 1))
     static let covidOrange               = UIColor(#colorLiteral(red: 0.9907451272, green: 0.409681201, blue: 0.3596678376, alpha: 1))
     static let covidHotPink              = UIColor(#colorLiteral(red: 0.9851650596, green: 0.3189211786, blue: 0.4142001271, alpha: 1))
     static let colorsArray               = [UIColor.black]
